903d4fd223f16f3c45e7fb12eb6c5bc74ee733b1
dhansmeier <121234076+dhansmeier@users.noreply.github.com>
2026-01-03 19:57:39 +0100

    new DB

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,7 @@
-create table tournament (
-  id bigserial primary key,
+create extension if not exists pgcrypto;
+
+create table tournaments (
+  id uuid primary key,
   name text not null,
   event_date date,
   created_at timestamptz not null default now()
